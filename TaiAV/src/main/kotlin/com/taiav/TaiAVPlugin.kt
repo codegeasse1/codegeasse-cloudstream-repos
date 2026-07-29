fa3ed48c299dae8b794a07a8eeacdcc6906e4b4a
codegeasse <chattered199@gmail.com>
2026-07-29 18:23:42 +0530

    Update TaiAVPlugin.kt

--- TaiAV/src/main/kotlin/com/taiav/TaiAVPlugin.kt
+++ TaiAV/src/main/kotlin/com/taiav/TaiAVPlugin.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class TaiAVPlugin: Plugin() {
+public class TaiAVPlugin : Plugin() {
     override fun load(context: Context) {
         registerMainAPI(TaiAVProvider())
     }
