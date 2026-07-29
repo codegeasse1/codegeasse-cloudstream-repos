c55fc712df086c64fd071c76f730d81a73f2dca7
codegeasse <chattered199@gmail.com>
2026-07-29 10:46:10 +0530

    Update ChikiAnimationPlugin.kt

--- ChikiAnimationProvider/src/main/kotlin/com/chikianimation/ChikiAnimationPlugin.kt
+++ ChikiAnimationProvider/src/main/kotlin/com/chikianimation/ChikiAnimationPlugin.kt
@@ -1,11 +1,12 @@
 package com.chikianimation
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
 
 @CloudstreamPlugin
-class ChikiAnimationPlugin : BasePlugin() {
-    override fun load() {
+class ChikiAnimationPlugin: Plugin() {
+    override fun load(context: Context) {
         registerMainAPI(ChikiAnimationProvider())
     }
 }
