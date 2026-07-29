8dd888ad29d3fb015fb6a2d096247b27bfb70d68
codegeasse <chattered199@gmail.com>
2026-07-29 14:40:39 +0530

    Update build.gradle.kts

--- AnimeKhorProvider/build.gradle.kts
+++ AnimeKhorProvider/build.gradle.kts
@@ -1,28 +1,37 @@
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension
-
 plugins {
     id("com.android.library")
     id("kotlin-android")
     id("com.lagradost.cloudstream3.gradle")
 }
 
+// use an integer for version numbers
+version = 1
+
+cloudstream {
+    // All of these properties are optional, you can safely remove them
+
+    description = "Watch Chinese Anime / Donghua from AnimeKhor"
+    authors = listOf("Codegeasse")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+
+    tvTypes = listOf("Anime")
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=animekhor.org&sz=%size%"
+}
+
 android {
     namespace = "com.animekhor"
     compileSdk = 33
-
+    
     defaultConfig {
         minSdk = 21
     }
-}
-
-cloudstream {
-    // Language tag
-    language = "en"
-    
-    // Status flag: 1 = Working, 2 = Down, 3 = Slow/Buggy
-    status = 1
-    
-    // Provider details
-    authors = listOf("Codegeasse")
-    description = "Watch Chinese Anime/Donghua English Subbed For Free Online from AnimeKhor"
 }
