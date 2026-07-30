bebba97a7349dcc6f0f3bee561ff62d979c50d42
codegeasse <chattered199@gmail.com>
2026-07-30 19:05:40 +0530

    Create build.gradle.kts

---
+++ AniKageProvider/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("com.lagradost.cloudstream3.gradle")
+}
+
+version = 1
+
+cloudstream {
+    description = "AniKage Extension"
+    authors = listOf("Codegeasse")
+    status = 1
+    tvTypes = listOf("Anime")
+    iconUrl = "https://www.google.com/s2/favicons?domain=anikage.cc&sz=%size%"
+}
+
+android {
+    namespace = "com.anikage"
+    compileSdk = 33
+    defaultConfig {
+        minSdk = 21
+    }
+}
