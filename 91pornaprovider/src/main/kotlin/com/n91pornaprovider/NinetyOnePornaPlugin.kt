84918bcc06364ad7a1a43285b66028afc59916f1
codegeasse <chattered199@gmail.com>
2026-07-31 03:59:16 +0530

    Create 91pornaprovider/src/main/kotlin/com/n91pornaprovider/NinetyOnePornaPlugin.kt

---
+++ 91pornaprovider/src/main/kotlin/com/n91pornaprovider/NinetyOnePornaPlugin.kt
@@ -0,0 +1,12 @@
+package com.n91pornaprovider
+
+import com.lagradost.cloudstream3.plugins.CloudStreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import com.lagradost.cloudstream3.plugins.registerMainAPI
+
+@CloudStreamPlugin
+class NinetyOnePornaPlugin: Plugin {
+    override fun load() {
+        registerMainAPI(NinetyOnePornaProvider())
+    }
+}
