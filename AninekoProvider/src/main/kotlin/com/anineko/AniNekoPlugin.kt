f1e72d6c79f7a80a432d21c73f6067b3ef73971b
codegeasse <chattered199@gmail.com>
2026-07-31 17:18:29 +0530

    Update AniNekoPlugin.kt

--- AninekoProvider/src/main/kotlin/com/anineko/AniNekoPlugin.kt
+++ AninekoProvider/src/main/kotlin/com/anineko/AniNekoPlugin.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import com.lagradost.cloudstream3.plugins.registerMainAPI
 
 @CloudStreamPlugin
-class AniNekoPlugin: Plugin() {
+class AniNekoPlugin : Plugin() {
     override fun load() {
         registerMainAPI(AniNekoProvider())
     }
