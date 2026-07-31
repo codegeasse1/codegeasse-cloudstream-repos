ce69c329ad941bfe0935b9c08f8d578ee0e85e3f
codegeasse <chattered199@gmail.com>
2026-07-31 16:53:59 +0530

    Create build.gradle.kts

---
+++ AninekoProvider/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("com.lagradost.cloudstream3.gradle")
+}
+
+android {
+    namespace = "com.anineko"
+    compileSdk = 33
+    defaultConfig {
+        minSdk = 21
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
+            freeCompilerArgs.addAll(
+                listOf(
+                    "-Xno-call-assertions",
+                    "-Xno-param-assertions",
+                    "-Xno-receiver-assertions"
+                )
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation("org.jsoup:jsoup:1.15.4")
+}
