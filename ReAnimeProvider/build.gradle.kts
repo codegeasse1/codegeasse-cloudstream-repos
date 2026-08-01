3319da4f4900822a1e46fc68aa091f47a8667871
codegeasse <chattered199@gmail.com>
2026-08-01 20:17:33 +0530

    Create build.gradle.kts

---
+++ ReAnimeProvider/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("com.lagradost.cloudstream3.gradle")
+}
+
+version = 1
+
+cloudstream {
+    description = "Re:Anime Extension"
+    authors = listOf("Codegeasse")
+    status = 1
+    tvTypes = listOf("Anime")
+    iconUrl = "https://www.google.com/s2/favicons?domain=reanime.to&sz=%size%"
+}
+
+android {
+    namespace = "com.reanime"
+    compileSdk = 33
+    defaultConfig {
+        minSdk = 21
+    }
+}
