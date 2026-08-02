dd6947638ce1790ef3ce0f37f80136a53e648645
codegeasse <chattered199@gmail.com>
2026-08-02 22:55:42 +0530

    Update build.gradle.kts

--- AniwavesProvider/build.gradle.kts
+++ AniwavesProvider/build.gradle.kts
@@ -1,5 +1,3 @@
-import com.lagradost.cloudstream3.gradle.CloudstreamExtensionConfiguration
-
 buildscript {
     repositories {
         google()
@@ -7,13 +5,11 @@ buildscript {
         maven("https://jitpack.io")
     }
     dependencies {
-        // Fetch the builder from the active recloudstream repository
         classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
     }
 }
 
-// CRITICAL FIX: The plugin ID remains lagradost to properly register the cloudstream{} block
 apply(plugin = "com.lagradost.cloudstream3.gradle")
 
 cloudstream {
