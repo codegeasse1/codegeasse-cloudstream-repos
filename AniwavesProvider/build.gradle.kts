f8a323cab684b050208df4972e6d7998dca85f2c
codegeasse <chattered199@gmail.com>
2026-08-02 23:10:42 +0530

    Update build.gradle.kts

--- AniwavesProvider/build.gradle.kts
+++ AniwavesProvider/build.gradle.kts
@@ -1,17 +1,30 @@
 plugins {
+    id("com.android.library")
+    id("kotlin-android")
     id("com.lagradost.cloudstream3.gradle")
 }
 
 cloudstream {
-    name = "Aniwaves"
-    description = "Aniwaves Extension"
-    version = 1
-    authors = listOf("Codegeasse")
-    mainProject = true
+}
+
+android {
+    namespace = "com.aniwaves"
+    compileSdk = 35
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 35
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 dependencies {
-    val cloudstreamApi = "com.github.recloudstream:cloudstream:master-SNAPSHOT"
+    val cloudstreamApi = "com.github.recloudstream.cloudstream:library:-SNAPSHOT"
     compileOnly(cloudstreamApi)
     implementation("org.jsoup:jsoup:1.15.3")
     implementation("org.json:json:20230227")
