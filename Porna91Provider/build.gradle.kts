075a0086a53fc1383c7e1e9b5c4c3c87ef66f970
codegeasse <chattered199@gmail.com>
2026-08-04 00:32:24 +0530

    Create build.gradle.kts

---
+++ Porna91Provider/build.gradle.kts
@@ -0,0 +1,14 @@
+cloudstream {
+    description = "91Porna Video Provider"
+    authors = listOf("Codegeasse")
+    status = 1
+    tvTypes = listOf("NSFW")
+    iconUrl = "https://www.google.com/s2/favicons?domain=91porna.com&sz=%size%"
+}
+
+android {
+    namespace = "com.pornea91"
+    defaultConfig {
+        minSdk = 21
+    }
+}
