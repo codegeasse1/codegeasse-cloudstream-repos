c6738b5cb5812a01b2514a2d304d00a1dd2ae0fe
codegeasse <chattered199@gmail.com>
2026-08-05 19:06:18 +0530

    Update LeakPornerPlugin.kt

--- LeakPornerProvider/src/main/java/com/leakporner/LeakPornerPlugin.kt
+++ LeakPornerProvider/src/main/java/com/leakporner/LeakPornerPlugin.kt
@@ -2,13 +2,10 @@ package com.leakporner
 
 import com.lagradost.cloudstream3.plugins.CloudStreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import com.lagradost.cloudstream3.ProviderType
 
 @CloudStreamPlugin
 class LeakPornerPlugin : Plugin {
     override fun loadPlugin() {
-        // Register the main provider
         registerMainAPI(LeakPornerProvider())
-        // If you later add extractors, register them here
     }
 }
