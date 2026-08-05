4b297275914a4e2ca81e0874d069c7b6999bf7b0
codegeasse <chattered199@gmail.com>
2026-08-05 17:40:24 +0530

    Add initial build configuration for PimpBunnyProvider

---
+++ PimpBunnyProvider/build.gradle.kts
@@ -0,0 +1,14 @@
+cloudstream {
+    description = "PimpBunny Video Provider"
+    authors = listOf("Codegeasse")
+    status = 1
+    tvTypes = listOf("NSFW")
+    iconUrl = "https://www.google.com/s2/favicons?domain=pimpbunny.com&sz=%size%"
+}
+
+android {
+    namespace = "com.pimpbunny"
+    defaultConfig {
+        minSdk = 21
+    }
+}
