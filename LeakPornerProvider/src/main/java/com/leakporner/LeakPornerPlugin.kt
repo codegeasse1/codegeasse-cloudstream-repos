7f33c0cb562467fb72c690214bc600db61c7f11d
codegeasse <chattered199@gmail.com>
2026-08-05 20:50:12 +0530

    Update LeakPornerPlugin.kt

--- LeakPornerProvider/src/main/java/com/leakporner/LeakPornerPlugin.kt
+++ LeakPornerProvider/src/main/java/com/leakporner/LeakPornerPlugin.kt
@@ -1,11 +1,13 @@
 package com.leakporner
 
-import com.lagradost.cloudstream3.plugins.CloudStreamPlugin
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
-@CloudStreamPlugin
-class LeakPornerPlugin : Plugin {
-    override fun loadPlugin() {
+@CloudstreamPlugin
+class LeakPornerPlugin : Plugin() {
+    override fun load(context: Context) {
+        // Registers the provider so CloudStream knows it exists
         registerMainAPI(LeakPornerProvider())
     }
 }
