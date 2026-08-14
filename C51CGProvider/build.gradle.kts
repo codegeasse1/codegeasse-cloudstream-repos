d82c88124f2fb3f0c0eff9bf16f1fe3a06e184f6
codegeasse <chattered199@gmail.com>
2026-08-14 21:26:30 +0530

    fix: restore full repo tree

---
+++ C51CGProvider/build.gradle.kts
@@ -0,0 +1,14 @@
+cloudstream {
+    description = "51CG Video Provider"
+    authors = listOf("Codegeasse")
+    status = 1
+    tvTypes = listOf("NSFW")
+    iconUrl = "https://www.google.com/s2/favicons?domain=51cg1.com&sz=%size%"
+}
+
+android {
+    namespace = "com.chigua"
+    defaultConfig {
+        minSdk = 21
+    }
+}
