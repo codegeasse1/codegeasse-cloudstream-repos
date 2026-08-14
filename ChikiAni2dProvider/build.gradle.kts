a4194fad3899474cdaa6b2d3b6dff0da128e7c9f
codegeasse <chattered199@gmail.com>
2026-08-15 01:27:38 +0530

    Add ChikiAni2dProvider/build.gradle.kts

---
+++ ChikiAni2dProvider/build.gradle.kts
@@ -0,0 +1,16 @@
+version = 1
+
+cloudstream {
+    description = "Watch Chinese Anime 2D / Donghua from ChikiAnimation"
+    authors = listOf("Codegeasse")
+    status = 1
+    tvTypes = listOf("Anime")
+    iconUrl = "https://www.google.com/s2/favicons?domain=chikianimation.com&sz=%size%"
+}
+
+android {
+    namespace = "com.chiki2d"
+    defaultConfig {
+        minSdk = 21
+    }
+}
