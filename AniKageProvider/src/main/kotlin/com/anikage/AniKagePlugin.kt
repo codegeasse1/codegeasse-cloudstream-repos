54df27ca59e8ccbb0e904e1c7ff7e5ca5ca2ddee
codegeasse <chattered199@gmail.com>
2026-08-14 21:25:42 +0530

    fix: restore full repo tree

---
+++ AniKageProvider/src/main/kotlin/com/anikage/AniKagePlugin.kt
@@ -0,0 +1,12 @@
+package com.anikage
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class AniKagePlugin: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(AniKageProvider())
+    }
+}
