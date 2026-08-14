be781bbb6b99ac822077f023af9c0aebaf6c74e7
codegeasse <chattered199@gmail.com>
2026-08-14 21:27:11 +0530

    fix: restore full repo tree

---
+++ KanAVProvider/src/main/kotlin/com/kanav/KanAVPlugin.kt
@@ -0,0 +1,12 @@
+package com.kanav
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class KanAVPlugin: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(KanAVProvider())
+    }
+}
