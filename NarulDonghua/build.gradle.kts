d67f069af744294ef462f30349c299072eaa2727
codegeasse <chattered199@gmail.com>
2026-08-14 21:28:46 +0530

    fix: restore full repo tree

---
+++ NarulDonghua/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("com.lagradost.cloudstream3.gradle")
+}
+
+version = 2
+
+cloudstream {
+    description = "Scraper for Narul Donghua"
+    authors = listOf("Codegeasse")
+    status = 1
+    tvTypes = listOf("Anime", "Movie")
+    iconUrl = "https://i2.wp.com/naruldonghua.com/wp-content/uploads/2023/12/cropped-Narul-Donghua-32x32.jpg"
+}
+
+android {
+    namespace = "com.naruldonghua"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 21
+    }
+}
+
+dependencies {
+    implementation("org.jsoup:jsoup:1.15.3")
+}
