64ac5fe3682f6b8ab40f44454032690d307e72c5
codegeasse <chattered199@gmail.com>
2026-08-14 21:27:34 +0530

    fix: restore full repo tree

---
+++ MisterDonghua/build.gradle.kts
@@ -0,0 +1,17 @@
+cloudstream {
+    description = "Watch Chinese Anime / Donghua from Mister Donghua"
+    authors = listOf("Codegeasse")
+
+    status = 1
+
+    tvTypes = listOf("Anime")
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=misterdonghua.com&sz=%size%"
+}
+
+android {
+    namespace = "com.misterdonghua"
+    defaultConfig {
+        minSdk = 21
+    }
+}
