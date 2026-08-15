c70a1ce794625125b2117b9c538b7a99ae082319
codegeasse <chattered199@gmail.com>
2026-08-16 05:11:55 +0530

    Add JustAnimeProvider extension (build.gradle.kts)

---
+++ JustAnimeProvider/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("com.lagradost.cloudstream3.gradle")
+}
+
+version = 1
+
+cloudstream {
+    description = "JustAnime Extension"
+    authors = listOf("Codegeasse")
+    status = 1
+    tvTypes = listOf("Anime", "AnimeMovie", "Movie", "TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=justanime.to&sz=%size%"
+}
+
+android {
+    namespace = "com.justanime"
+    compileSdk = 33
+    defaultConfig {
+        minSdk = 21
+    }
+}
+
+dependencies {
+    implementation("org.json:json:20230227")
+}
